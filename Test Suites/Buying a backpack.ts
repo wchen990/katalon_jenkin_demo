3224a17c50fb0f9c445f69362192ced5f4096fe1
Wei Chen <chenleeyuanwei@gmail.com>
2025-09-06 16:56:46 +0700

    Edit test suite

--- Test Suites/Buying a backpack.ts
+++ Test Suites/Buying a backpack.ts
@@ -5,10 +5,17 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>10</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>b67630b7-f6ab-403a-9947-041f5a9e3bae</testSuiteGuid>
+   <testCaseLink>
+      <guid>ed590048-b6ce-4c76-b36b-2109358828a5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Buying a backpack</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
