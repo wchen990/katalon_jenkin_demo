d2c5e9f10190682ff5516bbfdb69e42e1ce01cfa
Wei Chen <chenleeyuanwei@gmail.com>
2025-09-06 16:38:43 +0700

    Add Test Suite

---
+++ Test Suites/Buying a backpack.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Buying a backpack</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>b67630b7-f6ab-403a-9947-041f5a9e3bae</testSuiteGuid>
+</TestSuiteEntity>
